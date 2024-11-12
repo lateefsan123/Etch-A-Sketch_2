acfbd9f77785e0d14039af9c9adfed316208e431
lateefsan123 <lateefsanusifgc@gmail.com>
2024-11-12 17:54:24 +0000

    completed etch-a-sketch
    created reset button functionality, and popup message if wrong board size was created

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ let colorChoice = "black"
 const black = document.querySelector(".black");
 const random = document.querySelector(".random")
 const eraser = document.querySelector(".eraser")
+const reset = document.querySelector(".restart")
+let popup = document.querySelector(".popup")
+
+
 
 black.addEventListener("click", function() {
     colorChoice = "black";
@@ -20,6 +24,10 @@ const select = document.querySelector(".select")
 
 function createBoard() {
     size = +prompt("what width x height do you want the grids to be")
+    if (size > 100 || size < 0) {
+        popup.innerHTML = "Please enter a valid board size"
+    } else {
+    popup.innerHTML = "Lets start drawing!"
     const board = document.querySelector(".board");
 
     // Reset the board before creating a new grid
@@ -32,15 +40,16 @@ function createBoard() {
 
     for (let i = 0; i < whole; i++) {
         let div = document.createElement("div");
-        div.addEventListener("click", function() {
+        div.addEventListener("mouseover", function() {
             div.style.backgroundColor = colorChoice
         })
         board.insertAdjacentElement("beforeend", div)
 
     }
 }
+}
 
 select.addEventListener("click", createBoard)
-
+reset.addEventListener("click", createBoard)
 
 
